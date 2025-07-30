17da51375c324b06cbe9e0dbbe3177eb097841b9
seconp <15290631595@163.com>
2025-07-30 20:25:51 +0800

    讲字符串分割为数组

--- android/app/src/main/java/com/tomato/nativeaccessibility/NativeAccessibilityModule.java
+++ android/app/src/main/java/com/tomato/nativeaccessibility/NativeAccessibilityModule.java
@@ -30,7 +30,15 @@ public class NativeAccessibilityModule extends NativeAccessibilitySpec {
         // 这里可以调用无障碍服务来执行搜索操作
         // 例如，发送一个 Intent 或者直接调用服务的方法
         Log.d(NAME, "Performing search with query: " + query);
-        
+
+        // query会接收一个字符串，这个字符串可能是用/分开的，如果字符串使用/分开，将其拆分为数组
+        String[] queryParts;
+        if (query != null && query.contains("/")) {
+            queryParts = query.split("/");
+        } else {
+            // 如果 query 为 null 或者不包含 /，则将其视为单个搜索词
+            queryParts = new String[]{query};
+        }
         // 你可以在这里添加调用无障碍服务的逻辑
         // 比如通过发送广播或直接与 AccessibilityService 交互
     }
